674b14053e8eeb3bee02ba0d0d60ec0637267b62
duc.t.m.47@gmail.com <tranminhduc4796@gmail.com>
2019-01-20 17:06:26 +0700

    Update design for index page

--- basic_django/pythonweb/moviepoll/models.py
+++ basic_django/pythonweb/moviepoll/models.py
@@ -22,6 +22,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    choice_poster_link = models.CharField(max_length=200, default='Not available')
     votes = models.IntegerField(default=0)
 
     def __str__(self):
